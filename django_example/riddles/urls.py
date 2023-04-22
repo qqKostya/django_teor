12cc44419755a8c602a752d610943ef67a118c24
Kazakov Kostya <leap74rus@gmail.com>
2023-04-22 16:15:18 +0300

    fix: style code by django 4

--- django_example/riddles/urls.py
+++ django_example/riddles/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, DetailView, AnswerView
 
 app_name = "riddles"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("<int:riddle_id>/", DetailView.as_view(), name="detail"),
+    path("<int:riddle_id>/answer/", AnswerView.as_view(), name="answer"),
 ]
